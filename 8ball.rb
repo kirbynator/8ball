0a62c27aaa3d00331b1a6493e5d734015cfa0a4b
Zachary Kirby <i.am.kirbynator@gmail.com>
2019-02-14 13:59:20 -0700

    base done

---
+++ 8ball.rb
@@ -0,0 +1,16 @@
+@answers = []
+def start
+puts "What question would you like to ask the ball?"
+case = gets.strip.downcase
+when "quit"
+  puts "Come again"
+  sleep 3
+  print 'clear'
+  exit
+when "11"
+  admin_mode
+else 
+responce = Random(@anwers.count)
+puts "#{@answers[responce]"
+end
+end

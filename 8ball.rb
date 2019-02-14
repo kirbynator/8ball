77c0d274c969cba5598fc80867c491ef5a45cd22
Zachary Kirby <i.am.kirbynator@gmail.com>
2019-02-14 14:12:38 -0700

    lacking admin

--- 8ball.rb
+++ 8ball.rb
@@ -1,7 +1,7 @@
-@answers = []
+@answers = ["yes", "no", "maybe", "if you believe"]
 def start
 puts "What question would you like to ask the ball?"
-case = gets.strip.downcase
+case gets.strip.downcase
 when "quit"
   puts "Come again"
   sleep 3
@@ -10,7 +10,8 @@ when "quit"
 when "11"
   admin_mode
 else 
-responce = Random(@anwers.count)
+prng = Random.new
+responce = prng.rand(@answers.count)
 puts "#{@answers[responce]}"
 sleep 5
 start
